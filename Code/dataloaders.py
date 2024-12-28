9c9a42d975372da3404d561094773ed69c9b5ea9
Joel Tapia Salvador <91463596+NIU1638962@users.noreply.github.com>
2024-12-28 15:46:27 +0100

    Create dataloaders.py

---
+++ Code/dataloaders.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*- noqa
+"""
+Created on Sat Dec 28 15:44:49 2024
+
+@author: Joel Tapia Salvador
+"""
+from torch.utils.data import Dataset, DataLoader
+
+
+def create_dataloader(dataset: Dataset, batch: int) -> DataLoader:
+    """
+    Create a dataloader with the given dataset and batch size.
+
+    Parameters
+    ----------
+    dataset : Torch Dataset
+        Dataset that will be loaded.
+    batch : integer
+        Size of the batch that will be loaded each time.
+
+    Returns
+    -------
+    Torch DataLoader
+        Torch DataLoader that implements the batch itself.
+
+    """
+    if not issubclass(dataset, Dataset):
+        raise TypeError('"dataset" is not a subclass of Torch Dataset.')
+
+    return DataLoader(dataset, batch_size=batch, shuffle=True)
