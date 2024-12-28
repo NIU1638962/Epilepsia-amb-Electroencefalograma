39a78a9d1abd3c3f2fa53b576a1f050b60cca8d3
Joel Tapia Salvador <91463596+NIU1638962@users.noreply.github.com>
2024-12-28 16:12:21 +0100

    Create datasets.py

---
+++ Code/datasets.py
@@ -0,0 +1,155 @@
+# -*- coding: utf-8 -*- noqa
+"""
+Created on Sat Dec 28 15:27:34 2024
+
+@author: Joel Tapia Salvador
+"""
+
+from torch import from_numpy, Tensor
+from torch.utils.data import Dataset
+
+from load_datasets import load_seizures
+
+
+class SeizuresDataset(Dataset):
+    """Torch Dataset with the seizures information loaded."""
+
+    __slots__ = (
+        "__classes",
+        "__len",
+        "__path_root_directory",
+        "__patients_ids",
+        "__recordings",
+        "__windows",
+    )
+
+###############################################################################
+#                             Overloaded Operators                            #
+
+    def __init__(self, path_root_directory: str):
+        self.__path_root_directory = path_root_directory
+
+        self.__classes = Tensor()
+        self.__patients_ids = Tensor()
+        self.__recordings = Tensor()
+        self.__windows = Tensor()
+
+        self.__len = 0
+
+        self.__load_data()
+
+    def __getitem__(self, index: Tensor) -> Tensor:
+        """
+        Return a slice of the dataset.
+
+        Parameters
+        ----------
+        index : Torch Tensor
+            Indexes to return.
+
+        Returns
+        -------
+        Tensor
+            Slice of the dataset.
+
+        """
+        return self.__windows[index]
+
+    def __len__(self) -> int:
+        """
+        Length of the Dataset.
+
+        Returns
+        -------
+        integer
+            Length.
+
+        """
+        return self.__len
+
+###############################################################################
+
+
+###############################################################################
+#                              Protected Methods                              #
+
+    def __load_data(self): # noqa
+        windows, classes, patients_ids, recordings = load_seizures(
+            self.__path_root_directory
+        )
+
+        self.__windows = from_numpy(windows)
+        self.__classes = from_numpy(classes)
+        self.__patients_ids = from_numpy(patients_ids)
+        self.__recordings = from_numpy(recordings)
+
+        self.__len = len(self.__windows)
+
+###############################################################################
+
+
+###############################################################################
+#                                  Properties                                 #
+
+    @property # noqa
+    def classes(self) -> Tensor:
+        """
+        Retrive Torch Tensor with the classes of the windows.
+
+        Returns
+        -------
+        Torch Tensor
+            Classes of Windows. Is a mask 1 on 1 to the "windows" tensor.
+
+        """
+        return self.__classes
+
+    @property
+    def patients_ids(self) -> Tensor:
+        """
+        Retrive Torch Tensor with the Patient's IDs.
+
+        Returns
+        -------
+        Torch Tensor
+            Patient's IDs of Windows. Is a mask 1 on 1 to the "windows" tensor.
+
+        """
+        return self.__patients_ids
+
+    @property
+    def recordings(self) -> Tensor:
+        """
+        Retrive Torch Tensor with the Recordings of Patients of the Windows.
+
+        Returns
+        -------
+        Torch Tensor
+            Recordings of Patients of Windows. Is a mask 1 on 1 to the
+            "windows" tensor.
+
+        """
+        return self.__recordings
+
+    @property
+    def windows(self) -> Tensor:
+        """
+        Retrive Tensor with all the Windows with the data of seizures.
+
+        Returns
+        -------
+        Tensor
+            Windows of seizures.
+
+        """
+        return self.__windows
+
+###############################################################################
+
+
+if __name__ == "__main__":
+    dataset = SeizuresDataset('../Data/')
+    print(dataset.windows.shape)
+    print(dataset.classes.shape)
+    print(dataset.patients_ids.shape)
+    print(dataset.recordings.shape)
