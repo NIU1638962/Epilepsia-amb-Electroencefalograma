b4d6362a338cdb002763c86c30a7a10b441a7e0d
joanber024 <joansenior918273@gmail.com>
2025-01-13 21:30:13 +0100

    TEST BB

---
+++ Code/test_backbone.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*- noqa
+"""
+Created on Sun Dec 29 18:22:30 2024
+
+@author: Joan
+"""
+
+import torch
+
+from torch.nn import CrossEntropyLoss
+from datasets import SeizuresDataset
+from environ import DATA_PATH, DEBUG
+from models import EpilepsyLSTMBB, FeatureLevelFusion, get_hyperparameters
+from utils import echo
+from kfold import generalized_model_patient_kfold, personalized_model_record_kfold, test_BB
+
+
+def main():
+    """
+    Contain main logic.
+
+    Returns
+    -------
+    None.
+
+    """
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    if DEBUG:
+        echo(f'Device: "{device}"')
+
+    models = {
+        'BB': {
+            'model': FeatureLevelFusion,
+            'optimizer': torch.optim.Adam,
+            'num_epochs': 50,
+        },
+        'LSTM': {
+            'model': EpilepsyLSTMBB,
+            'optimizer': torch.optim.Adam,
+            'num_epochs': 15,
+        }
+    }
+
+    echo('')
+
+    echo('READING DATASET')
+
+    data = SeizuresDataset(DATA_PATH)
+
+    echo('DATASET READ')
+
+    loss_func = CrossEntropyLoss()
+
+    batch_size = 1024
+
+    window_batch = 1024
+
+    model_params = get_hyperparameters(config=1)
+
+    echo('')
+    echo('--Generalized Model--')
+
+    test_BB(
+        data,
+        models,
+        loss_func,
+        batch_size,
+        window_batch,
+        device,
+        model_params,
+    )
+
+
+if __name__ == '__main__':
+    main()
