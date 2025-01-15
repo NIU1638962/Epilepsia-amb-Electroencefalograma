0651a74b8ebb2e1a1ea9a0f3c066889dd83f0032
Joel Tapia Salvador <91463596+NIU1638962@users.noreply.github.com>
2025-01-15 13:26:15 +0100

    Update environ.py

--- Code/environ.py
+++ Code/environ.py
@@ -15,6 +15,9 @@ PLATFORM = sys.platform.lower()
 
 USER = ''
 
+CODE_PATH = os.path.dirname(os.path.abspath(__file__))
+PROJECT_PATH = os.path.dirname(CODE_PATH)
+
 if PLATFORM == 'linux':
     USER = os.getenv('USER')
 elif PLATFORM == 'win32':
@@ -26,24 +29,30 @@ elif PLATFORM == 'darwin':
 USER.lower()
 
 if USER == 'maed02':
-    DATA_PATH = '../../../maed/EpilepsyDataSet/'
-    PICKLE_PATH = '../Pickle'
-    RESULTS_PATH = '../Results/'
-    TRAINED_MODELS_PATH = '../Trained Models/'
+    DATA_PATH = os.path.join(
+        os.path.dirname(os.path.dirname(PROJECT_PATH)),
+        'maed',
+        'EpilepsyDataSet',
+    )
+    PICKLE_PATH = os.path.join(PROJECT_PATH, 'Pickle')
+    RESULTS_PATH = os.path.join(PROJECT_PATH, 'Results')
+    TRAINED_MODELS_PATH = os.path.join(PROJECT_PATH, 'Trained Models')
 elif USER == 'Usuario':
-    DATA_PATH = "../EpilepsyDataSet"
-    PICKLE_PATH = '../Pickle'
-    RESULTS_PATH = '../Results/'
-    TRAINED_MODELS_PATH = '../Trained Models/'
+    DATA_PATH = os.path.join(PROJECT_PATH, 'EpilepsyDataSet')
+    PICKLE_PATH = os.path.join(PROJECT_PATH, 'Pickle')
+    RESULTS_PATH = os.path.join(PROJECT_PATH, 'Results')
+    TRAINED_MODELS_PATH = os.path.join(PROJECT_PATH, 'Trained Models')
 else:
-    DATA_PATH = '../Data/'
-    PICKLE_PATH = '../Pickle'
-    RESULTS_PATH = '../Results/'
-    TRAINED_MODELS_PATH = '../Trained Models/'
+    DATA_PATH = os.path.join(PROJECT_PATH, 'Data')
+    PICKLE_PATH = os.path.join(PROJECT_PATH, 'Pickle')
+    RESULTS_PATH = os.path.join(PROJECT_PATH, 'Results')
+    TRAINED_MODELS_PATH = os.path.join(PROJECT_PATH, 'Trained Models')
 
 if DEBUG:
     echo(f'Platform: "{PLATFORM}"')
     echo(f'User: "{USER}"')
+    echo(f'Path to project: "{PROJECT_PATH}"')
+    echo(f'Path to code: "{CODE_PATH}"')
     echo(f'Path to data: "{DATA_PATH}"')
     echo(f'Path to pickle: "{PICKLE_PATH}"')
     echo(f'Path to results: "{RESULTS_PATH}"')
